c4b6edf55df262cffbd3ceb713b00008d981a82b
Manoj <your-email@gmail.com>
2026-06-27 21:21:27 +0530

    Updated filename and fixed changes

---
+++ day2.js
@@ -0,0 +1,21 @@
+// const manoj = 20;
+// // manoj = 21;
+// console.log(manoj);
+
+// const peron = {
+//     name = "manoj"
+// }
+
+// peron.name = "rajput";
+
+// console.log(peron);
+
+const person = {
+    name: "Alice",
+    age: 25
+};
+
+person.age = 26;
+person.name = "Rajput";
+
+console.log(person);
---
+++ day3.js
@@ -0,0 +1,14 @@
+let score;
+console.log(score);
+let phone = null;
+console.log(null);
+let id = Symbol("id");
+console.log(id);
+
+let manoj1 = {
+    name: "manoj Thakur",
+    age: 22
+};
+console.log(manoj1);
+
+console.log(typeof(id));
