4e63152757f2404bd715a381c8450f49afefb1b3
Manoj <your-email@gmail.com>
2026-06-27 21:53:03 +0530

    Added new folder

---
+++ day6_comparision.js
@@ -0,0 +1,18 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+
+console.log(null == 0);
+console.log(null > 0);
+console.log(null < 0);
+
+
+
+
+
+
+
+console.log("04" === 4);
+console.log("04" == 4);
