cfc7c119b94b0a0c9ee6144f6ea922e7860b843a
Manoj <your-email@gmail.com>
2026-06-27 21:35:09 +0530

    Updated project

---
+++ day4(conversiondatatype).js
@@ -0,0 +1,24 @@
+// let score = "stri";
+
+// console.log(typeof(score));
+// console.log(typeof score);
+
+
+
+// let value = Number(score);
+// console.log(typeof(value));
+// console.log(value);
+
+
+
+let login = 1;
+console.log(typeof(login));
+let num = Boolean(1);
+console.log(num);
+console.log(typeof(num));
+
+
+let somenumber = 22;
+let stingNumber = String(somenumber)
+console.log(typeof(stingNumber));
+console.log(stingNumber);
