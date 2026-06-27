0858d09db81af41f9a74beccfc55a9101c69f369
Manoj <your-email@gmail.com>
2026-06-27 21:44:46 +0530

    Updated project

---
+++ day5_(conversionOperator).js
@@ -0,0 +1,17 @@
+let value = 3
+let negvalue = -value
+
+// console .log (negative value);
+console.log(negvalue)
+console.log(2 + 2 + 3 + '5');
+console.log('4' + '6' + 5);
+console.log('3' + 4);
+console.log(5 + '10');
+
+// boolean value.
+
+
+console.log(true);
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+console.log(num1);
