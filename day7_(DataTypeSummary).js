1e42af89af27cb85e98026d9c7504ab6052eeba9
Manoj <your-email@gmail.com>
2026-06-27 22:13:19 +0530

    Added datatype summary and updated file

---
+++ day7_(DataTypeSummary).js
@@ -0,0 +1,56 @@
+// primitve data tyepe.
+
+// 1. String
+// 2. Number
+// 3. null
+// 4. Symbol
+// 5. undefined
+// 6. Boolean
+// 7. BigInt.
+
+
+// Non primitive data type.
+// 1. objet
+// 2. function
+// 3. map
+// 4. array
+// 5. set
+
+
+const score = 100
+const scoreValue = 100
+
+
+const isLoggedIn = false
+const outsideTemp = null
+
+
+const id = Symbol("123")
+const anotherId = Symbol("234")
+
+console.log(id)
+let bigNumber = 235234325235235232342
+
+
+
+
+
+//array
+
+const heros = ["shaktima", "maderchod", "bolmaderchod"]
+
+// objet
+manoj = {
+    name: "manojrana",
+    age: 30,
+    roll: 23
+}
+
+
+// fucntion
+myfunction = function() {
+    console.log("hii iam fucniton");
+}
+
+console.log(typeof myfunction);
+myfunction();
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,13 @@
-et id = Symbol("id");
-consol
+const score = 100
+// const scoreValue = 100
+
+
+// const isLoggedIn = false
+// const outsideTemp = null
+
+
+// const id = Symbol("123")
+// const anotherId = Symbol("234")
+
+// console.log(id)
+// let bigNumber = 235234325235235232342
