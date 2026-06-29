0a194d15cb3d0bb2874865af7d970450b12617b4
Manoj <your-email@gmail.com>
2026-06-30 02:10:20 +0530

    Updated project

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,4 +1,52 @@
-let a = 33;
+let a = 156e5;
+// let b = 156e-5;
+// console.log(a);
+// console.log(b);
+
+
+// let a = 999999999999999;
+// let b = 9999999999999999;
+// console.log(a);
+// console.log(b);
+
+
+// let x = 0.22 + 0.12;
+// let y = (0.22 * 10 + 0.12 * 10) / 10;
+// console.log(x);
+// console.log(y);
+// console.log(0 / 0);
+// console.log(isNaN("hello"));
+
+// console.log(1 / 0);
+// console.log(-1 / 0);
+// console.log(isFinite(100)); // Adding two numbers
+// let x = 10;
+// let y = 15;
+// let z = x + y;
+// console.log(z);
+
+// // Concatenating two strings:
+
+// let a = "10";
+// let b = "30";
+// let c = a + b;
+// console.log(c);
+
+// let x = "100" / "10";
+// let y = "100" * "10";
+// let z = "100" - "10";
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+
+
+
+
+
+
+// // Decimal Number
+// let a = 33;
 // let b = 3.3;
 // console.log(a);
 // console.log(b);
@@ -10,3 +58,29 @@ let a = 33;
 // let y = 0B0111;
 // console.log(x);
 // console.log(y);
+
+
+// let x = 0xfff;
+// console.log(x);
+
+// const total = null + 5;
+// console.log(total); // 5
+
+
+
+// const n1 = true + 10;
+// const n2 = false + 10;
+
+// console.log(n1);
+// console.log(n2);
+
+// const s1 = '42';
+// const s2 = 'hello';
+
+// const strToNum1 = Number(s1);
+// const strToNum2 = Number(s2);
+
+// console.log(strToNum1);
+// console.log(strToNum2);
+
+
