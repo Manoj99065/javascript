dfef0f7eacf46c049d1a986d0c08b547ba02d79e
Manoj <your-email@gmail.com>
2026-06-30 11:24:53 +0530

    Updated project

---
+++ day11_math.js
@@ -0,0 +1,45 @@
+//Math libray.
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(5.8));
+
+// console.log(Math.min(4, 3, 2, 5, 23));
+// console.log(Math.random() * 10);
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.random() * 10 + 1);
+
+// const min = 10;
+// const max = 20;
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// console.log(Math.log(10));
+
+// console.log(Math.sin(Math.PI / 2));
+// console.log(Math.cos(0));
+// console.log(Math.tan(Math.PI / 4))
+
+// // inverse Treignomatry ?
+
+// console.log(Math.asin(1));
+// console.log(Math.acos(1));
+// console.log(Math.atan(1));
+// console.log(Math.atan2(min, max));
+
+
+// // Hyperbolic fucntion.
+// Math.sinh()
+// Math.cosh()
+// Math.tanh()
+// Math.asinh()
+// Math.acosh()
+// Math.atanh()
+
+// other useful method.
+
+console.log(Math.fround(5.123456789));
+console.log(Math.fround(5.123456789));
+console.log(Math.imul(4, 5)); // 20)
+
+console.log(Math.clz32(1));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,32 @@
+// console.log(Math.ceil(5.8));
+
+// console.log(Math.min(4, 3, 2, 5, 23));
+// console.log(Math.random() * 10);
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.random() * 10 + 1);
+
+// const min = 10;
+// const max = 20;
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// console.log(Math.log(10));
+
+// console.log(Math.sin(Math.PI / 2));
+// console.log(Math.cos(0));
+// console.log(Math.tan(Math.PI / 4))
+
+// // inverse Treignomatry ?
+
+// console.log(Math.asin(1));
+// console.log(Math.acos(1));
+// console.log(Math.atan(1));
+// console.log(Math.atan2(min, max));
+
+
+// // Hyperbolic fucntion.
+// Math.sinh()
+// Math.cosh()
+// Math.tanh()
+// Math.asinh()
+// Math.acosh()
